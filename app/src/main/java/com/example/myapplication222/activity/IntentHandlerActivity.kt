f34ba3b63f8f34ccb25f79d67df132d9700b9052
СТихотворение <kolpakovstanislav3@gmail.com>
2024-03-01 11:41:19 +0300

    edit

---
+++ app/src/main/java/com/example/myapplication222/activity/IntentHandlerActivity.kt
@@ -0,0 +1,33 @@
+package com.example.myapplication222.activity
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_INDEFINITE
+import com.example.myapplication222.databinding.ActivityIntentHandlerBinding
+import com.google.android.material.snackbar.Snackbar
+import com.example.myapplication222.R
+class IntentHandlerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityIntentHandlerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        intent?.let {
+            if (it.action != Intent.ACTION_SEND) {
+                return@let
+            }
+
+            val text = it.getStringExtra(Intent.EXTRA_TEXT)
+            if (text.isNullOrBlank()) {
+                Snackbar.make(binding.root, R.string.error_empty_content, LENGTH_INDEFINITE)
+                    .setAction(android.R.string.ok) {
+                        finish()
+                    }
+                    .show()
+                return@let
+            }
+            // TODO: handle text
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/myapplication222/activity/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.myapplication222.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.result.launch
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication222.R
+import com.example.myapplication222.adapter.OnInteractionListener
+import com.example.myapplication222.adapter.PostsAdapter
+import com.example.myapplication222.databinding.ActivityMainBinding
+import com.example.myapplication222.Post
+import com.example.myapplication222.viewModel.PostViewModel
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val viewModel: PostViewModel by viewModels()
+
+        val adapter = PostsAdapter(object : OnInteractionListener {
+            override fun onEdit(post: Post) {
+                viewModel.edit(post)
+            }
+
+            override fun onLike(post: Post) {
+                viewModel.likeById(post.id)
+            }
+
+            override fun onRemove(post: Post) {
+                viewModel.removeById(post.id)
+            }
+
+            override fun onShare(post: Post) {
+                val intent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    putExtra(Intent.EXTRA_TEXT, post.content)
+                    type = "text/plain"
+                }
+
+                val shareIntent =
+                    Intent.createChooser(intent, getString(R.string.chooser_share_post))
+                startActivity(shareIntent)
+            }
+        })
+        binding.list.adapter = adapter
+        viewModel.data.observe(this) { posts ->
+            adapter.submitList(posts)
+        }
+
+        val newPostLauncher = registerForActivityResult(NewPostResultContract()) { result ->
+            result ?: return@registerForActivityResult
+            viewModel.changeContent(result)
+            viewModel.save()
+        }
+
+        binding.fab.setOnClickListener {
+            newPostLauncher.launch()
+        }
+    }
+}
+
+
+
+
